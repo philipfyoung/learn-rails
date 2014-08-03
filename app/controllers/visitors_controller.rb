963f268e771e4e879efac300282aa3d7311eff64
Philip Young <Philip.Young@yale.edu>
2014-08-03 21:05:12 +0000

    Update main page with css

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,8 @@
 class VisitorsController < ApplicationController
   def new
+    Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + "\033[1;34;40m[Controller called]\033[0m "
     @owner = Owner.new
+    flash[:notice] = "Hello Wolrd"
+    flash.now[:alert]  = "Oh Boy!"
   end
 end
